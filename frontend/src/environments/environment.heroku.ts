641ebb759f2c722059215efa4148ff4693df653d
ben <bholsinger@gmail.com>
2025-12-01 15:40:12 -0700

    Configure frontend for Heroku with production backend URL

---
+++ frontend/src/environments/environment.heroku.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://devops-dashboard-api-bh-47f1eb8a5435.herokuapp.com/api',
+  hubUrl: 'https://devops-dashboard-api-bh-47f1eb8a5435.herokuapp.com/dashboardHub'
+};
